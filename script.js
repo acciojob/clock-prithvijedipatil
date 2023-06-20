91faece1ea27e89b645c31d2e361e8fda446e62a
prithvijedipatil <prithvipatil09@gmail.com>
2023-06-20 15:42:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+window.onload = () => {
+
+    setInterval(update,1000);
+}
+
+function update(){
+
+    const disp = document.getElementById('timer');
+    const curr = new Date();
+
+    const dateop = { weekday :'long' , year:'numeric', month:'long', day:'numeric'};
+    const timeop = {hour :'numeric', minute:'numeric',second:'numeric',hour12:true};
+
+    const date1 = curr.toLocaleDateString(undefined,dateop);
+    const time1 = curr.toLocaleTimeString(undefined,timeop);
+
+    disp.innerHTML = `${date1} , ${time1}`;
+}
